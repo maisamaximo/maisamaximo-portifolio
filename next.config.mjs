890c34ab9bb0c431e67a2b4dee95933bdb258e00
maisamaximo <ferreira.maximo.maisa@gmail.com>
2024-06-30 01:24:00 +0100

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
+// next.config.mjs
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/maisamaximo-portifolio/" : "",
+  assetPrefix: isProd ? "/maisamaximo-portifolio/" : "",
   images: {
     unoptimized: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
